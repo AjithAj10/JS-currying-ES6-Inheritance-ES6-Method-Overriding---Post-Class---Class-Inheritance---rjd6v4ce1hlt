491b60ee193e9293340beef392f05b9118d4ca46
ajith <ajithraveendranr@gmail.com>
2022-06-20 00:06:56 -0700

    first

--- src/components/circle.js
+++ src/components/circle.js
@@ -1,6 +1,8 @@
 // import using require
-
+const shapeClass = reuire('./shape');
 // declare class
-
-
+class Circle extends shapeClass{
+    calculateArea();
+}
 // export class using module.exports
+module.export = shapeClass;
--- src/components/shape.js
+++ src/components/shape.js
@@ -1,3 +1,8 @@
 // declare class
-
+class Shape{
+    drawShape();
+    calculateArea();
+     color = "red";
+}
 // export class using module.exports
+module.export = Shape
