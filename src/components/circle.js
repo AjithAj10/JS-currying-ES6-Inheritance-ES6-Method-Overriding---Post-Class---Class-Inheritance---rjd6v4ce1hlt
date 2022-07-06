87621aede8f95a3c313c2890e54bdef127dc2f01
AjithAj10 <104498680+AjithAj10@users.noreply.github.com>
2022-07-06 09:38:31 -0700

    Update circle.js

--- src/components/circle.js
+++ src/components/circle.js
@@ -1,11 +1,14 @@
 // import using require
-const shapeClass = reuire('./shape');
+const shapeClass = require('./shape');
 // declare class
 class Circle extends shapeClass{
+    constructor(){
+
+    this.transform = 5;
+    }
     calculateArea(){
        
-    };
-    transform = 5;
+    };   
 }
 // export class using module.exports
 module.export = shapeClass;
